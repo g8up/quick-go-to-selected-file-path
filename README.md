c4cc8cef7ba4bd86980723228b89b215b3511183
g8up <g8up@qq.com>
2017-12-27 21:47:15 +0800

    init repo

---
+++ README.md
@@ -0,0 +1,9 @@
+# quickpick-selected-text
+
+This Extension help you quick jump to your selected file path.
+
+It just executes `workbench.action.quickOpen` command with the text you selected.
+
+You can config a shortcut for it, eg. `Ctrl+E`.
+
+**Enjoy!**
---
+++ src/extension.ts
@@ -0,0 +1,37 @@
+'use strict';
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+
+function getSelection(){
+    // current editor
+    const editor = vscode.window.activeTextEditor;
+
+    // check if there is no selection
+    if (editor.selection.isEmpty) {
+        // the Position object gives you the line and character where the cursor is
+        return '';
+    }
+    return editor.document.getText(editor.selection);
+}
+// console.log('launched: sigma ')
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+    // The command has been defined in the package.json file
+    // Now provide the implementation of the command with  registerCommand
+    // The commandId parameter must match the command field in package.json
+    let disposable = vscode.commands.registerCommand('extension.quickPickSelectedText', () => {
+        // The code you place here will be executed every time your command is executed
+
+        // Display a message box to the user
+        vscode.commands.executeCommand('workbench.action.quickOpen', getSelection() );
+        // vscode.window.showInformationMessage(getSelection());
+    });
+
+    context.subscriptions.push(disposable);
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {
+}
